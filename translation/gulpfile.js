4924190cddbd735ac204f9545f63d5fdb4071cbe
Hellon Canella <helloncanella@gmail.com>
2016-11-22 01:26:54 -0200

    translation

---
+++ translation/gulpfile.js
@@ -0,0 +1,17 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var reload      = browserSync.reload;
+
+// Static server
+gulp.task('default', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    }); 
+
+  gulp.watch(["*.html", "js/*.js"]).on("change", reload);
+});
+
+
+
