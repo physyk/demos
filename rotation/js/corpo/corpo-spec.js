fd4a53b6996064945c3a1d3e9590925839f32784
Hellon Canella <helloncanella@gmail.com>
2016-12-29 23:00:27 -0200

    end of day

--- rotation/js/corpo/corpo-spec.js
+++ rotation/js/corpo/corpo-spec.js
@@ -1,13 +1,35 @@
+// function setupDependecies(){
+//     function 
+
+//     return {Corpo}
+// }
+
+function createCorpo({gravidade, corda, vertices, massa}){
+    gravidade = gravidade || 10
+    corda = corda || new (function Corda(){})()
+    verctices = vertices || new Vector()
+
+    return 
+}
+
 describe('Corpo', function(){
-    it('pode ser rotacionado', function(){
+    it('tem seu centro de massa definido pelos vertices', function(){
 
     })
 
-    it('pode ser transladado', function(){
+    it('a atualização das forças recalcula e a atualiza as forças aplicadas', function(){
 
     })
 
-    it('sofrea a aplicação de forças', function(){
-        
+    it('a atualização dos torques recalcula e a atualiza os torques aplicados', function(){
+
+    })
+
+    it('a translação move por igual todas os vertices', function(){
+
+    })
+
+    it('a rotação move por igual rotaciona todos os vertices por igual', function(){
+
     })
 })
